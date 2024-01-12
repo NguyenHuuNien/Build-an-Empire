53ee2da05a3e6ab76de92b8308eaa3a05dd780ff
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-01-12 08:17:55 +0700

    game up

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public int Mau;
+    public int SatThuong;
+    public int TocDoDiChuyen;
+    public int TocDoDanh;
+    public float TamDanh;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Hero
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private bool isCom;
+    private bool trongTamDanh;
+    private GameObject targetObject;
+    private float timeWait;
+    private void Update() {
+        Debug.DrawLine(transform.position,transform.position + (isCom?-1:1) * Vector3.right * TamDanh,Color.white);
+        trongTamDanh = Check();
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(!trongTamDanh){
+            Move();
+        }else{
+            rb.velocity = Vector2.zero;
+            Attack();
+        }
+    }
+    private void Attack(){
+        if(timeWait >= TocDoDanh){
+            targetObject.GetComponent<Player>().Mau -= this.SatThuong;
+            timeWait = 0;
+        }else{
+            timeWait += Time.fixedDeltaTime * 1.2f;
+        }
+    }
+    private void Move(){
+        rb.velocity = Vector2.right * this.TocDoDiChuyen * (isCom?-1:1);
+    }
+    private bool Check(){
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,transform.right,this.TamDanh,layerMask);
+        targetObject = hit.collider!=null ? hit.collider.gameObject : null;
+        return hit.collider != null;
     }
 }
